98744f80672d5ddc38aa9acbec4a99d61a3bc384
Gustut Biksa <gustutbiksa@gmail.com>
2026-06-02 07:56:05 +0800

    wip: connect menu service to hero card

--- src/components/hero/HeroCard.tsx
+++ src/components/hero/HeroCard.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import "../../styles/heroSection.css";
+import type { Menu } from "../../types/menu";
 
 export default function HeroCard() {
+  const [card, setCard] = useState<Menu[]>([]);
+
   return (
     <div className="hero-mid-card">
       <article className="card-1-api"></article>
-      <article className="card-2-api"></article>
-      <article className="card-3-api"></article>
-      <article className="card-4-api"></article>
+
+      {/* MULAI CODING  DARI SINI, CEK CHAT GPT, SEKARANG INTINYA AMBIL DATA MENUS UNTUK DITAMPILKAN MENJADI CARD DI DASHBOARD HERO SECTION */}
     </div>
   );
 }
